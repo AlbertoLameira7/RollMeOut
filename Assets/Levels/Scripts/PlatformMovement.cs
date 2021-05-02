9d72c5c7a5141a1544a8356e6d29a7e51320db24
Alberto Lameira <lameira.alberto94@gmail.com>
2021-05-02 15:20:38 +0100

    Player Movement, Installed URP

---
+++ Assets/Levels/Scripts/PlatformMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private Vector3 _vec;
+    private Quaternion _startRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _startRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputZ = Input.GetAxis("Horizontal") * 10f * -1;
+        float inputX = Input.GetAxis("Vertical") * 10f;
+
+        transform.rotation = Quaternion.Euler(inputX, 0, inputZ) * _startRotation;
+    }
+}
---
+++ Assets/Player/Scripts/Gravity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NS_Gravity
+{
+    public class Gravity : MonoBehaviour
+    {
+        [SerializeField] private float _gravityMultiplier = 1.0f;
+        private float _acceleration = 9.8f;
+        private Rigidbody _rigidBody;
+        private bool _isGravityActive = true;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _rigidBody = gameObject.GetComponent<Rigidbody>();
+
+            if (_rigidBody == null)
+
+            {
+                Debug.Log("No RigidBody Attached to GameObject");
+            }
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            // Apply gravity if object isn't on ground and gravity is enabled
+            if (_isGravityActive)
+            {
+                float accel = (_acceleration * Time.fixedDeltaTime * _gravityMultiplier) * -1;
+                _rigidBody.velocity = new Vector3(_rigidBody.velocity.x, _rigidBody.velocity.y + accel, _rigidBody.velocity.z);
+            }
+        }
+    }
+}
