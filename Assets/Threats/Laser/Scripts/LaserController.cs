9c99b069eaa019e115da09d9cc7d6069a4b5fe0d
Alberto Lameira <lameira.alberto94@gmail.com>
2021-05-04 21:45:16 +0100

    Added Laser Trap

---
+++ Assets/Threats/Laser/Scripts/LaserController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 2f;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up * _rotationSpeed * Time.fixedDeltaTime);
+    }
+}
